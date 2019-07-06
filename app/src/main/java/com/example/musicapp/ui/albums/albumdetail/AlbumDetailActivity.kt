91b4bad6b557ddedfa92032f5a144db61a9352ab
safal <safalkumarghimire@gmail.com>
2019-07-06 13:15:26 +0200

    Created project and Ui skeletons

---
+++ app/src/main/java/com/example/musicapp/ui/albums/albumdetail/AlbumDetailActivity.kt
@@ -0,0 +1,13 @@
+package com.example.musicapp.ui.albums.albumdetail
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.musicapp.R
+
+class AlbumDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/example/musicapp/ui/albums/albumslist/AlbumListActivity.kt
@@ -0,0 +1,13 @@
+package com.example.musicapp.ui.albums.albumslist
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.musicapp.R
+
+class AlbumListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
