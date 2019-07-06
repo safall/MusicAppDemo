f8f2b0efb20bbbd07b13839cb307eebd3db92a27
safal <safalkumarghimire@gmail.com>
2019-07-06 14:16:26 +0200

    Implemented api fetch in repo and service

--- app/src/main/java/com/example/musicapp/data/network/DataRepo.kt
+++ app/src/main/java/com/example/musicapp/data/network/DataRepo.kt
@@ -1,5 +1,15 @@
 package com.example.musicapp.data.network
 
+import com.example.musicapp.data.model.Artist
+import com.example.musicapp.data.model.BaseResponse
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
 import javax.inject.Inject
 
-class DataRepo @Inject constructor(private val service: RemoteService)
+class DataRepo @Inject constructor(private val service: RemoteService){
+
+    fun searchArtists(offset: Int, query: String): Single<BaseResponse<List<Artist>>> {
+        return service.searchArtists(query, offset)
+            .observeOn(AndroidSchedulers.mainThread())
+    }
+}
--- app/src/main/java/com/example/musicapp/data/network/RemoteService.kt
+++ app/src/main/java/com/example/musicapp/data/network/RemoteService.kt
@@ -1,8 +1,17 @@
 package com.example.musicapp.data.network
 
+import com.example.musicapp.data.model.Artist
+import com.example.musicapp.data.model.BaseResponse
+import io.reactivex.Single
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface RemoteService {
 
     companion object {
         const val ENDPOINT = "https://api.deezer.com/"
     }
+
+    @GET("search/artist?")
+    fun searchArtists(@Query("q") query: String, @Query("index") index: Int): Single<BaseResponse<List<Artist>>>
 }
